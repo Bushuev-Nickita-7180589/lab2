8be9ce885e6b79f4ae4d466a42432a43dfde1372
Bushuev-Nickita-7180589 <bushuev.nickita.7180589@gmail.com>
2026-03-02 11:25:01 +0500

    Initial commit

---
+++ lab2_1/Program.fs
@@ -0,0 +1,30 @@
+﻿//Бушуев Никита Николаевич БАС-1(2024)
+//Вариант 2
+open System
+
+let readList() =
+    printf "Введите количество элементов: "
+    let n = int(Console.ReadLine())
+    
+    let rec readElements acc count =
+        if count = 0 then
+            List.rev acc
+        else
+            printf "Введите элемент %d: " (n - count + 1)
+            let x = int(Console.ReadLine())
+            readElements (x::acc) (count - 1)
+    
+    readElements [] n
+
+let lastDigit x = abs x % 10
+
+[<EntryPoint>]
+let main argv =
+
+    let numbers = readList()
+    printfn "Исходный список: %A" numbers
+    
+    let lastDigits = List.map lastDigit numbers
+    printfn "Последние цифры: %A" lastDigits
+    
+    0
---
+++ lab2_2/Program.fs
@@ -0,0 +1,37 @@
+﻿//Бушуев Никита Николаевич БАС-1(2024)
+//Вариант 2
+open System
+
+let readList() =
+    printf "Введите количество элементов: "
+    let n = int(Console.ReadLine())
+    
+    let rec readElements acc count =
+        if count = 0 then
+            List.rev acc
+        else
+            printf "Введите элемент %d: " (n - count + 1)
+            let x = int(Console.ReadLine())
+            readElements (x::acc) (count - 1)
+    
+    readElements [] n
+
+[<EntryPoint>]
+let main argv =
+    let numbers = readList()
+    printfn "Исходный список: %A" numbers
+    
+    printf "Введите цифру (0-9), на которую должны оканчиваться элементы: "
+    let digit = int(Console.ReadLine()) % 10
+    
+    let sum = 
+        List.fold (fun acc x -> 
+            if x % 10 = digit then 
+                acc + x 
+            else 
+                acc 
+        ) 0 numbers
+    
+    printfn "Сумма элементов, оканчивающихся на %d: %d" digit sum
+    
+    0
